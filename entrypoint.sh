cc64e54fa5115785c7a66de6812ef2fd662846d8
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-04-21 22:58:17 +0200

    removed cp from entrypoint, PATH is enough

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # fix for installation failures on nodejs 8.11.1
-export PATH=$PATH:/home/node/node_modules/phantomjs-prebuilt/lib/phantom/bin/
+export PATH=$PATH:/home/node/node_modules/phantomjs-prebuilt/lib/phantom/bin
 if ! [ -x "$(command -v phantomjs)" ]; then
 	echo "initial install phantomjs"
 	cd "${NODE_HOME}"
 	export NODE_TLS_REJECT_UNAUTHORIZED=0
 	npm install phantomjs-prebuilt
-	sudo cp /home/node/node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs /usr/local/bin/
 	cd "${APP_HOME}"
 fi
 
